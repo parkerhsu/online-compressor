4214549c9f89775cdbd0fb90e83e25342ea315dd
akiyamer <1019892846@qq.com>
2022-08-09 19:03:02 +0800

    feat(web): 添加Upload组件样式

---
+++ packages/web/src/components/Progress/index.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { Progress as AntProgress } from 'antd'
+import './index.less'
+
+interface Props {
+  type: 'line' | 'infinite'
+  style?: React.CSSProperties
+}
+
+const Progress: React.FC<Props> = (props) => {
+  const { type, style } = props
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setProgress(100)
+    }, 200)
+  }, [])
+
+  return type === 'line' ? (
+    <div style={{ width: '130px' }}>
+      <AntProgress percent={progress} status="active" showInfo={false} />
+    </div>
+  ) : (
+    <div className="progress-bar" style={style}></div>
+  )
+}
+
+export default Progress
--- packages/web/src/components/Upload/index.tsx
+++ packages/web/src/components/Upload/index.tsx
@@ -1,43 +1,71 @@
-import * as React from 'react'
-import { Button, Upload, message } from 'antd'
+import React, { useState, useRef } from 'react'
+import { Button, Upload, message, Slider } from 'antd'
+import Progress from '@/components/Progress'
 import type { UploadProps } from 'antd'
-import { PlusOutlined } from '@ant-design/icons'
+import { PlusOutlined, PlusCircleFilled, ArrowDownOutlined } from '@ant-design/icons'
+import { v4 as uuid } from 'uuid'
 import './index.less'
 
 interface Props {}
 
-const { Dragger } = Upload
+type FileItem = {
+  key: string
+  file: File
+}
+
+enum CompressStatus {
+  WAIT = 1,
+  COMPRESSING = 2,
+  FINISHED = 3
+}
 
 const UploadComponent: React.FC<Props> = (props) => {
-  const inputRef = React.useRef<HTMLInputElement>()
-
-  const uploadProps: UploadProps = {
-    name: 'file',
-    multiple: true,
-    action: '/api/file/upload',
-    onChange(info) {
-      const { status } = info.file
-      if (status !== 'uploading') {
-        console.log(info.file, info.fileList)
-      }
-      if (status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully.`)
-      } else if (status === 'error') {
-        message.error(`${info.file.name} file upload failed.`)
-      }
-    },
-    onDrop(e) {
-      console.log('Dropped files', e.dataTransfer.files)
-    }
-  }
+  const [showUploadList, setShowUploadList] = useState(false)
+  const [fileList, setFileList] = useState<FileItem[]>([])
+  const [compressStatus, setCompressStatus] = useState<CompressStatus>(CompressStatus.WAIT)
+  const inputRef = useRef<HTMLInputElement>()
+  const progressType = compressStatus === CompressStatus.COMPRESSING ? 'infinite' : 'line'
 
   function triggerInputClick() {
     inputRef.current?.click()
   }
 
   function handleUpload(e: any) {
-    const files = e.target.files
-    console.log(files)
+    if (!showUploadList) setShowUploadList(true)
+
+    let files = e.target.files
+    files = Array.from(files)
+    const filesArr = files.map((file: File) => ({
+      key: uuid(),
+      file
+    }))
+    setFileList([...fileList, ...filesArr])
+  }
+
+  function beginCompress() {}
+
+  function formatFileSize() {}
+
+  function renderFileList() {
+    return fileList.map((item) => (
+      <div className="list-item" key={item.key}>
+        <div className="file-name">
+          <span>{item.file.name}</span>
+        </div>
+        <span>1.89MB</span>
+        <Progress type={progressType} />
+        {compressStatus !== CompressStatus.FINISHED && (
+          <span>{compressStatus === CompressStatus.WAIT ? '等待上传' : '处理中'}</span>
+        )}
+        {compressStatus === CompressStatus.FINISHED && (
+          <div className="finished-info">
+            <span>684KB</span>
+            <span>-64%</span>
+            <ArrowDownOutlined className="download-icon" />
+          </div>
+        )}
+      </div>
+    ))
   }
 
   return (
@@ -50,15 +78,41 @@ const UploadComponent: React.FC<Props> = (props) => {
         ref={inputRef as any}
         onChange={handleUpload}
       />
-      <div className="upload-inner" onClick={triggerInputClick}>
-        <div className="plus-box">
-          <PlusOutlined className="icon" />
+      {!showUploadList ? (
+        <div className="upload-inner" onClick={triggerInputClick}>
+          <div className="plus-box">
+            <PlusOutlined className="icon" />
+          </div>
+          <p className="h1">图片压缩</p>
+          <p className="sub-tip">（最多上传 30 个文件，单个图片最大为 25 M，支持jpg、png格式）</p>
         </div>
-        <p className="h1">图片压缩</p>
-        <p className="sub-tip">（最多上传 30 个文件，单个图片最大为 25 M，支持jpg、png格式）</p>
-      </div>
-
-      
+      ) : (
+        <div className="compress-container">
+          <div className="header">
+            <span>压缩强度</span>
+            <div className="slider-wrapper">
+              <Slider marks={{ 0: '轻度', 50: '普通', 100: '强力' }} step={null} defaultValue={0} />
+            </div>
+            <Button type="primary" onClick={beginCompress}>
+              开始压缩
+            </Button>
+          </div>
+          <div className="list-wrapper">{renderFileList()}</div>
+          <div className="bottom">
+            {compressStatus !== CompressStatus.FINISHED ? (
+              <Button onClick={triggerInputClick}>
+                <PlusCircleFilled />
+                继续上传
+              </Button>
+            ) : (
+              <div className="bottom-info">
+                <span>共 1 个文件，完成压缩 1 个， 1.89 MB → 684 KB，共计压缩 64%</span>
+                <Button>清除队列</Button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
