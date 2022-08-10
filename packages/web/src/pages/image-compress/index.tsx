b5d3c3cbb357b2af180a7f0079cfc2fd141de857
zyh <1796172146@qq.com>
2022-08-10 22:02:03 +0800

    压缩页面背景颜色

--- packages/web/src/pages/image-compress/index.tsx
+++ packages/web/src/pages/image-compress/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Upload from '@/components/Upload'
+import './index.css'
+
 
 function ImageCompress() {
   return (
     <div>
+      <div className='bg'></div>
       <Upload />
     </div>
   )
