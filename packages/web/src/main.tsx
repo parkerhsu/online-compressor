abdc75a85d44fc9abea12eb2a8a6c215e104188f
J*2L <1468793144@qq.com>
2022-08-10 11:01:10 +0800

    主页静态页面

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import '@/static/css/reset.css'
+import '@/static/css/common.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
